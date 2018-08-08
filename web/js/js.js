7c862df770c4b86c842aa389c8f7299e1c46ab76
isi <isi.yma@gmail.com>
2018-08-08 17:10:39 +0900

    とりあえず1段目１０個までチェック、Textarea内に01でON部を出力まで。

--- web/js/js.js
+++ web/js/js.js
@@ -1,12 +1,35 @@
 $(function(){
   var btn = $('button');
+  var e = document.getElementById ('dataview');
+  e.value = 'hoge';
   btn.click(function(){
+    var a = document.getElementsByClassName('btn');
+    var id =  $(this).attr("name");
     //btn.removeClass('active');
     if ($(this).hasClass('active')) {
       $(this).removeClass('active');
-
     }else{
-      $(this).addClass('active');      
+      $(this).addClass('active');
     }
+    //e.value=a[0].outerHTML;
+    //console.log(a);
+
+    var s='';
+    for(var i=0;i<10;i++){
+      var s1=String(a[i].outerHTML);
+      //var s1=String(a[i].);
+      var result = s1.indexOf('active');
+      console.log(s1,result);
+
+      if(result>0){
+        console.log('Hit');
+        s=s+'1';
+      }else{
+        s=s+'0';
+      }
+      //console.log(a);
+      console.log(i,s);
+    }
+    e.value=s;
   });
 });
