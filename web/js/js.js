6fcdc292c0efa963f315a52701dde0e6f7f86a32
isi <isi.yma@gmail.com>
2018-09-10 19:32:48 +0900

    CSV ファイルボタンList化修正

--- web/js/js.js
+++ web/js/js.js
@@ -1,4 +1,4 @@
-//Project Hata 用JS
+//Project Hata 用JavaScript
 
 function waitTextValueCng($this) {
     console.log($this.value);
@@ -10,71 +10,84 @@ function waitTextValueCng($this) {
 
 }
 
-function cpFileBtnOn($this) {
   // Copyボタン押されると呼び出される。csvFileNameTextの値をボタンの値に
+  function cpFileBtnOn($this) {
+  // ※リスト化したので未使用※
     console.log($this.value);
     document.getElementById("csvFileNameText").value = $this.value;
 
-    // *.csvをリスト化してプルダウンリストにしてもよいかも？（未実装）
 }
 
 
+//全体のイベントドリブン
 $(
-  // シーケンサーマトリクスクリックチェック用
 
   function(){
-  var btn = $('button');
-  var e = document.getElementById ('dataview');
-  var stat = document.getElementById ('stat');
-  var waitSec = document.getElementById ('WaitTime');
 
-  //console.log(stat,waitSec);
-
-  //e.value = 'hoge';
-  btn.click(function(){
-    var a = document.getElementsByClassName('btn');
-    var name =  $(this).attr("name"); //name拾っているが使ってない
-    //btn.removeClass('active');
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active');
-    }else{
-      $(this).addClass('active');
+    // CSVリストが選択されたらValueをcsvFileNameTextテキストボックスへ
+    var select = document.getElementById( 'listbox' );
+    select.onchange = function()
+    {
+      // 選択されているoption要素を取得する
+      var selectedItem = this.options[ this.selectedIndex ];
+      document.getElementById("csvFileNameText").value = selectedItem.value;
+      console.log( selectedItem.value );
     }
-    //e.value=a[0].outerHTML;
-    //console.log(a);
 
-    var s='';
 
-    //var s2=String(waitSec.outerHTML);
-    var s2=waitSec.value;
-      console.log('Wait=',s2);
+    // 以下シーケンサーマトリクスクリックチェック用
+    var btn = $('button');
+    var e = document.getElementById ('dataview');
+    var stat = document.getElementById ('stat');
+    var waitSec = document.getElementById ('WaitTime');
+    //console.log(stat,waitSec);
 
-      //s=s+s2+'\n';
-      s=s2+'\n';
 
-      for(var i=0;i<(20*6);i++){
-        if(!(i % 20)){
-          if (i !=0 ){
-            s=s.slice(0,-1) // 最後の , カット
-            s=s+'\n';
-          }
-        }
-
-        var s1=String(a[i].outerHTML);
-        //var s1=String(a[i].);
-        var result = s1.indexOf('active');
-        console.log(s1,result);
-
-        if(result>0){
-          console.log('Hit');
-          s=s+'1,';
-        }else{
-          s=s+'0,';
-        }
-        //console.log(a);
-        console.log(i,s);
+    //e.value = 'hoge';
+    btn.click(function(){
+      var a = document.getElementsByClassName('btn');
+      var name =  $(this).attr("name"); //name拾っているが使ってない
+      //btn.removeClass('active');
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+      }else{
+        $(this).addClass('active');
       }
-    s=s.slice(0,-1) // 最後の , カット
-    e.value=s;
+      //e.value=a[0].outerHTML;
+      //console.log(a);
+
+      var s='';
+
+      //var s2=String(waitSec.outerHTML);
+      var s2=waitSec.value;
+        console.log('Wait=',s2);
+
+        //s=s+s2+'\n';
+        s=s2+'\n';
+
+        for(var i=0;i<(20*6);i++){
+          if(!(i % 20)){
+            if (i !=0 ){
+              s=s.slice(0,-1) // 最後の , カット
+              s=s+'\n';
+            }
+          }
+
+          var s1=String(a[i].outerHTML);
+          //var s1=String(a[i].);
+          var result = s1.indexOf('active');
+          console.log(s1,result);
+
+          if(result>0){
+            console.log('Hit');
+            s=s+'1,';
+          }else{
+            s=s+'0,';
+          }
+          //console.log(a);
+          console.log(i,s);
+        }
+      s=s.slice(0,-1) // 最後の , カット
+      e.value=s;
   });
 });
